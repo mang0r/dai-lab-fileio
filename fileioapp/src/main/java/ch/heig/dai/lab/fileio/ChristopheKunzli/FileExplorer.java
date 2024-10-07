13a46c31f2114f4add0abbccfeae60515f0379eb
ChristopheKunzli <christophe.kunzli@cpnv.ch>
2024-10-07 15:55:20 +0200

    Implement FileExplorer.java

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/ChristopheKunzli/FileExplorer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/ChristopheKunzli/FileExplorer.java
@@ -1,6 +1,7 @@
 package ch.heig.dai.lab.fileio.ChristopheKunzli;
 
 import java.io.File;
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 
 public class FileExplorer {
@@ -25,7 +26,14 @@ public class FileExplorer {
      * @return a new file, or null if there is no new file
      */
     public File getNewFile() {
-        // TODO: implement the method body here
+        File[] files = folder.listFiles();
+        assert files != null;
+        for (File file : files) {
+            if (!knownFiles.contains(file)) {
+                knownFiles.add(file);
+                return file;
+            }
+        }
         return null;
     }
 }
