abc6c9e11fe98797e0754ba784f160ed30498265
SDY <dylan.stegmuller@heig-vd.ch>
2024-10-07 17:07:03 +0200

    EncodingSelector 9r55hs

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/_9r55hs/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/_9r55hs/EncodingSelector.java
@@ -2,6 +2,9 @@ package ch.heig.dai.lab.fileio._9r55hs;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 public class EncodingSelector {
 
@@ -17,7 +20,15 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
-        return null;
+        Dictionary<String, Charset> extensions = new Hashtable<>();
+        extensions.put("utf8",      StandardCharsets.UTF_8);
+        extensions.put("txt",       StandardCharsets.US_ASCII);
+        extensions.put("utf16be",   StandardCharsets.UTF_16BE);
+        extensions.put("utf16le",   StandardCharsets.UTF_16LE);
+
+        String[] splitFilename = file.toString().split("\\.");
+        String fileExt = splitFilename[splitFilename.length - 1];
+
+        return extensions.get(fileExt);
     }
 }
