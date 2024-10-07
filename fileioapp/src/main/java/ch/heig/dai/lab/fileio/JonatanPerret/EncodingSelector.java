a5c8be29bd0097ebb4217884634efcb3a0368ac4
Jonatan Perret <perretjonatan1@gmail.com>
2024-10-07 16:11:09 +0200

    encoding selector

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/JonatanPerret/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/JonatanPerret/EncodingSelector.java
@@ -2,9 +2,17 @@ package ch.heig.dai.lab.fileio.JonatanPerret;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.util.HashMap;
 
 public class EncodingSelector {
 
+    // dictionary of supported encodings
+    private static final HashMap<String, String> supportedEncoding = new HashMap<String, String>() {{
+        put(".utf8", "UTF-8");
+        put(".txt", "US-ASCII");
+        put(".utf16be", "UTF-16BE");
+        put(".utf16le", "UTF-16LE");
+    }};
     /**
      * Get the encoding of a file based on its extension.
      * The following extensions are recognized:
@@ -17,7 +25,12 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
+        // get the extension of the file
+        String extension = file.getName().substring(file.getName().lastIndexOf('.'));
+        // return null if the extension is not recognized
+        if (supportedEncoding.containsKey(extension)) {
+            return Charset.forName(supportedEncoding.get(extension));
+        }
         return null;
     }
 }
