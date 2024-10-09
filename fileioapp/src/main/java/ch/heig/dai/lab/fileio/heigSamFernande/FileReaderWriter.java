0aa714a89e4562247de2a56b3d5dce99502f2b7e
heigSamFernande <samuel.fernandez@heig-vd.ch>
2024-10-09 18:57:52 +0200

    FileReaderWriter for heigSamFernande

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/heigSamFernande/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/heigSamFernande/FileReaderWriter.java
@@ -11,12 +11,34 @@ public class FileReaderWriter {
      * @param encoding
      * @return the content of the file as a String, or null if an error occurred.
      */
-    public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
-        return null;
+    public String readFile(File file, Charset encoding) throws IOException 
+    {
+        // If no file or encoding, return null
+        if(file == null || encoding == null)
+            return null;
+
+        StringBuilder result = new StringBuilder();
+
+        try (BufferedReader fileToRead = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding))) 
+        {
+            String line;
+
+            // Read and add to result the lines of the file
+            while ((line = fileToRead.readLine()) != null) 
+            {
+                result.append(line).append("\n");
+            }
+            
+        } 
+        catch (IOException e) 
+        {
+            System.out.println("Exception : " + e);
+            return null;
+        }
+
+        return result.toString();
     }
+
 
     /**
      * Write the content to a file with a given encoding. 
@@ -25,10 +47,24 @@ public class FileReaderWriter {
      * @param encoding the encoding to use
      * @return true if the file was written successfully, false otherwise
      */
-    public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+    public boolean writeFile(File file, String content, Charset encoding) 
+    {
+        // If one parameter null, return false
+        if(file == null || content == null || encoding == null)
+            return false;
+
+        try (BufferedWriter fileToWrite = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding)))
+        {
+            fileToWrite.write(content);
+            fileToWrite.flush();
+
+            return true;
+        } 
+        catch (IOException e) 
+        {
+            System.out.println("Exception : " + e);
+            return false; // False if error
+        }
     }
+
 }
