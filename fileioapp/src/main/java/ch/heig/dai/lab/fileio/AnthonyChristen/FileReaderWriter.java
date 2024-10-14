5f5612e5d1f298e4c453871f3c34c5e8819a3863
Anthony Christen <anthony1998@bluewin.ch>
2024-10-14 16:17:31 +0200

    FileReaderWriter implemented for Anthony-Christen

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/AnthonyChristen/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/AnthonyChristen/FileReaderWriter.java
@@ -12,9 +12,19 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding))) {
+            StringBuilder result = new StringBuilder();
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.append(line).append("\n");
+            }
+
+            return result.toString();
+        } catch (IOException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
         return null;
     }
 
@@ -26,9 +36,15 @@ public class FileReaderWriter {
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding))) {
+            writer.write(content);
+            writer.flush();
+
+            return true;
+        } catch (IOException e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+
         return false;
     }
 }
