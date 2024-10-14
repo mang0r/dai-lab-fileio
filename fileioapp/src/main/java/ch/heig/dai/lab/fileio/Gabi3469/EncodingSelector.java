26108c0d6600c1c315826053c15d2141d44d8056
Gabriel Barros Fernandes <gabriel.barrosfernandes@heig-vd.ch>
2024-10-14 16:09:02 +0200

    Add Code Encoding Selector for Gabi3469

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Gabi3469/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Gabi3469/EncodingSelector.java
@@ -18,6 +18,20 @@ public class EncodingSelector {
      */
     public Charset getEncoding(File file) {
         // TODO: implement the method body here
+        String fileName = file.getName();
+        String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+        switch (fileExtension) {
+            case "utf8":
+                return Charset.forName("UTF-8");
+            case "txt":
+                return Charset.forName("US-ASCII");
+            case "utf16be":
+                return Charset.forName("UTF-16BE");
+            case "utf16le":
+                return Charset.forName("UTF-16LE");
+            default:
+                break;
+        }
         return null;
     }
 }
