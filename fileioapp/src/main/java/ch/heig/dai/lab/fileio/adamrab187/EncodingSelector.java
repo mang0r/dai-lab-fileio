a24110a68774d4b2c9df62ac6f2f07db9bba2a4e
adamrab187 <adam.raboud@hes-so.ch>
2024-10-14 15:35:20 +0200

    add comments

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/adamrab187/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/adamrab187/EncodingSelector.java
@@ -18,21 +18,27 @@ public class EncodingSelector {
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
+        // Get the name of the file
         String fileName = file.getName();
+
+        // Find the last occurrence of the dot character in the file name
         int lastDotIndex = fileName.lastIndexOf('.');
 
+        // If a dot is found and it is not the last character in the file name
         if (lastDotIndex != -1) {
+            // Extract the file extension
             String extension = fileName.substring(lastDotIndex + 1);
 
+            // Return the corresponding Charset based on the file extension
             return switch (extension) {
                 case "utf8" -> StandardCharsets.UTF_8;
                 case "txt" -> StandardCharsets.US_ASCII;
                 case "utf16be" -> StandardCharsets.UTF_16BE;
                 case "utf16le" -> StandardCharsets.UTF_16LE;
-                default -> null;
+                default -> null; // Return null if the file extension is not recognized
             };
         }
-
+        // If no dot is found or it is the last character in the file name return null
         return null;
     }
 }
