8a0293fc687d90f5c499894fa73184bf48eb1510
AxelRochat <axel.rochat05@gmail.com>
2024-10-14 16:47:30 +0200

    Transformer Axel Rochat

--- fileioapp/src/test/java/ch/heig/dai/lab/fileio/axelrochat/TransformerTest.java
+++ fileioapp/src/test/java/ch/heig/dai/lab/fileio/axelrochat/TransformerTest.java
@@ -13,21 +13,21 @@ public class TransformerTest {
         private String testString = "Chuck Norris s’est fait flasher à 280km/h sur l’autoroute. A pied.";
 
     @Test
-    @Disabled
+    // @Disabled
     public void replaceChuckTest() {
         String expected = "Jean-Claude Van Damme s’est fait flasher à 280km/h sur l’autoroute. A pied.";
         assertEquals(expected, transformer.replaceChuck(testString));
     }
 
     @Test
-    @Disabled
+    // @Disabled
     public void noReplaceChuckTest() {
         String testString = "Ce texte ne contient pas le nom de Chuck N.";
         assertEquals(testString, transformer.replaceChuck(testString));
     }
 
     @Test
-    @Disabled
+    // @Disabled
     public void capitalizeWordsTest() {
         String expected = "Chuck Norris S’est Fait Flasher À 280km/h Sur L’autoroute. A Pied.";
         assertEquals(expected, transformer.capitalizeWords(testString));
