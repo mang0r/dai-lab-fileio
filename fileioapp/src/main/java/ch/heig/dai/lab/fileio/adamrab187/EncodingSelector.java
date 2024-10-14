ec21735d3a64f5941bc5249ba7fe0ed1e299a41d
adamrab187 <adam.raboud@hes-so.ch>
2024-10-14 15:26:55 +0200

    add feature encoding selector

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/adamrab187/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/adamrab187/EncodingSelector.java
@@ -2,6 +2,7 @@ package ch.heig.dai.lab.fileio.adamrab187;
 
 import java.io.File;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class EncodingSelector {
 
@@ -12,12 +13,26 @@ public class EncodingSelector {
      *   - .txt:     US-ASCII
      *   - .utf16be: UTF-16BE
      *   - .utf16le: UTF-16LE
-     * 
+     *
      * @param file the file to get the encoding from
      * @return the encoding of the file, or null if the extension is not recognized
      */
     public Charset getEncoding(File file) {
-        // TODO: implement the method body here
+        String fileName = file.getName();
+        int lastDotIndex = fileName.lastIndexOf('.');
+
+        if (lastDotIndex != -1) {
+            String extension = fileName.substring(lastDotIndex + 1);
+
+            return switch (extension) {
+                case "utf8" -> StandardCharsets.UTF_8;
+                case "txt" -> StandardCharsets.US_ASCII;
+                case "utf16be" -> StandardCharsets.UTF_16BE;
+                case "utf16le" -> StandardCharsets.UTF_16LE;
+                default -> null;
+            };
+        }
+
         return null;
     }
 }
