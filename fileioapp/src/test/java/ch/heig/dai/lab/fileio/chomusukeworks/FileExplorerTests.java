44ec8383a8a7e837ca2e25cb5272a6040aff8a83
Chomusuke <yukhanov.artur@gmail.com>
2024-10-14 14:48:26 +0200

    Enabled unit test

--- fileioapp/src/test/java/ch/heig/dai/lab/fileio/chomusukeworks/FileExplorerTests.java
+++ fileioapp/src/test/java/ch/heig/dai/lab/fileio/chomusukeworks/FileExplorerTests.java
@@ -1,8 +1,6 @@
-// TODO change this to your own package instead of jehrensb ***
 package ch.heig.dai.lab.fileio.chomusukeworks;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Disabled;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,7 +11,6 @@ import java.util.Set;
 public class FileExplorerTests {
 
 	@Test
-	@Disabled
 	public void getNewFilesTest() throws IOException {
 		// Create a temporary directory under the default temp directory using java.io.File
 
