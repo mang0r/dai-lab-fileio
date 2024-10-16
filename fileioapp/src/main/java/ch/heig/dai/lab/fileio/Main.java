63ef1cba013a44bf5f1eb21cfe80c853d5b6f821
JeffMv <JeffMv@users.noreply.github.com>
2024-10-16 19:52:51 +0200

    Added Main files processing + output directory

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Main.java
@@ -1,13 +1,16 @@
 package ch.heig.dai.lab.fileio;
 
 import java.io.File;
+import java.nio.charset.Charset;
 
 // *** TODO: Change this to import your own package ***
-import ch.heig.dai.lab.fileio.jehrensb.*;
+import ch.heig.dai.lab.fileio.JeffMv.*;
+
+import javax.xml.crypto.dsig.Transform;
 
 public class Main {
     // *** TODO: Change this to your own name ***
-    private static final String newName = "Edison";
+    private static final String newName = "Jeffrey";
 
     /**
      * Main method to transform files in a folder.
@@ -24,6 +27,10 @@ public class Main {
      */
     public static void main(String[] args) {
         // Read command line arguments
+        boolean DEBUG = true;
+        if (DEBUG) {
+            args = new String[]{"/Users/user/Projets/hes-repos/hes-dai-lab-fileio/jokes", "4"};
+        }
         if (args.length != 2 || !new File(args[0]).isDirectory()) {
             System.out.println("You need to provide two command line arguments: an existing folder and the number of words per line.");
             System.exit(1);
@@ -33,13 +40,52 @@ public class Main {
         System.out.println("Application started, reading folder " + folder + "...");
         // TODO: implement the main method here
 
+        FileExplorer explorer = new FileExplorer(folder);
+
+
+
         while (true) {
             try {
                 // TODO: loop over all files
+                File file = explorer.getNewFile();
+                System.out.println(file.getName());
+                if (file == null || !file.exists()) {
+                    break;
+                }
+                EncodingSelector encSelector = new EncodingSelector();
+                Charset encoding = encSelector.getEncoding(file);
+                FileReaderWriter fileHandler = new FileReaderWriter();
+                Transformer transformer = new Transformer(newName, wordsPerLine);
+
+                String content = fileHandler.readFile(file, encoding);
+                content = transformer.capitalizeWords(content);
+                content = transformer.replaceChuck(content);
+                content = transformer.wrapAndNumberLines(content);
+
+                String destPath = destinationFilepath(file);
+                // String destPath = file.getAbsolutePath() + ".processed";
+                fileHandler.writeFile(new File(destPath), content, encoding);
 
             } catch (Exception e) {
                 System.out.println("Exception: " + e);
+                // raise(e);
             }
         }
+    }
+
+
+    private static String destinationFilepath(File file) {
+        return makeDestinationDirectory(file) + "/" + file.getName() + ".processed";
+    }
+
+    private static String makeDestinationDirectory(File file) {
+        String path = getFilePath(file) + "/processed";
+        File dir = new File(path);
+        dir.mkdirs();
+        return path;
+    }
+
+    private static String getFilePath(File file) {
+        return file.getParent();
     }
 }
