81fd9da633d5f9a79af893e5c0ff6299de9a5e11
hliosone <sstelcher@protonmail.com>
2024-10-20 21:41:47 +0200

    File explorer implemented with tests

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/hliosone/FileExplorer.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/hliosone/FileExplorer.java
@@ -26,6 +26,19 @@ public class FileExplorer {
      */
     public File getNewFile() {
         // TODO: implement the method body here
+        if (folder.isDirectory() && folder.exists()){
+            File[] files = folder.listFiles();
+
+            if (files != null){
+                for (File file : files){
+                    if (!knownFiles.contains(file)){
+                        knownFiles.add(file);
+                        return file;
+                    }
+                }
+            }
+        }
+
         return null;
     }
 }
