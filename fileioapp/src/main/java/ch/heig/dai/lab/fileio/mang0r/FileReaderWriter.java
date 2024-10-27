29072b0c34b3c947a54030cd4ce4c975716e2888
mang0r <benoit.jaouen@heig-vd.ch>
2024-10-27 16:03:11 +0100

    FileReaderWriter implemented, added tests for this feature

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/mang0r/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/mang0r/FileReaderWriter.java
@@ -1,7 +1,13 @@
 package ch.heig.dai.lab.fileio.mang0r;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.Charset;
+import java.util.stream.Collectors;
 
 public class FileReaderWriter {
 
@@ -15,7 +21,12 @@ public class FileReaderWriter {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to close the streams and readers at the end.
-        return null;
+        try (var r = new BufferedReader (new InputStreamReader(
+                new FileInputStream(file), encoding));) {
+            return r.lines().collect(Collectors.joining("\n"));
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     /**
@@ -29,6 +40,12 @@ public class FileReaderWriter {
         // TODO: Implement the method body here. 
         // Use the ...Stream and ...Reader classes from the java.io package.
         // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+        try (var o = new BufferedOutputStream(new FileOutputStream(file.getAbsolutePath()));) {
+            o.write(content.getBytes());
+            o.flush();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
