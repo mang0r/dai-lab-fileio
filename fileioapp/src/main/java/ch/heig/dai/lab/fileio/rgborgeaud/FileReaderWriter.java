632da1dceaf17b38772b0c894286c51763d13248
rgborgeaud <gael.borgeaud@heig-vd.ch>
2024-10-27 08:53:06 +0100

    FileReaderWriter for rgborgeaud

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/rgborgeaud/FileReaderWriter.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/rgborgeaud/FileReaderWriter.java
@@ -12,10 +12,23 @@ public class FileReaderWriter {
      * @return the content of the file as a String, or null if an error occurred.
      */
     public String readFile(File file, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to close the streams and readers at the end.
-        return null;
+        try(BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(file),
+                        encoding))) {
+
+            String str, text = "";
+
+            while((str = reader.readLine()) != null) {
+                text = text.concat(str + "\n");
+            }
+
+            return text;
+
+        } catch(IOException e) {
+            System.out.println("Exception : " + e);
+            return null;
+        }
     }
 
     /**
@@ -26,9 +39,18 @@ public class FileReaderWriter {
      * @return true if the file was written successfully, false otherwise
      */
     public boolean writeFile(File file, String content, Charset encoding) {
-        // TODO: Implement the method body here. 
-        // Use the ...Stream and ...Reader classes from the java.io package.
-        // Make sure to flush the data and close the streams and readers at the end.
-        return false;
+        try (var writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        new FileOutputStream(file),
+                        encoding
+                ))) {
+
+            writer.write(content);
+            return true;
+
+        } catch (IOException e) {
+            System.out.println("Exception : " + e);
+            return false;
+        }
     }
 }
