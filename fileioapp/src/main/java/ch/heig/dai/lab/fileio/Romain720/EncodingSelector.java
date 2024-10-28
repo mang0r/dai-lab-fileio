badc425a53d199d1ded5d8941d02d80a53a78aca
Romain <romain.hurni@hes-so.ch>
2024-10-28 10:44:13 +0100

    EncodingSelector for Romain720

--- fileioapp/src/main/java/ch/heig/dai/lab/fileio/Romain720/EncodingSelector.java
+++ fileioapp/src/main/java/ch/heig/dai/lab/fileio/Romain720/EncodingSelector.java
@@ -18,6 +18,29 @@ public class EncodingSelector {
      */
     public Charset getEncoding(File file) {
         // TODO: implement the method body here
+        String fName = file.getName();
+        String fExtension = fName.substring(fName.lastIndexOf('.') + 1);
+
+        switch (fExtension) {
+
+            case "utf8":
+
+                return Charset.forName("UTF-8");
+
+            case "txt":
+
+                return Charset.forName("US-ASCII");
+
+            case "utf16be":
+
+                return Charset.forName("UTF-16BE");
+
+            case "utf16le":
+
+                return Charset.forName("UTF-16LE");
+
+        }
+
         return null;
     }
 }
